fc89fa0cdcb4e9520aa71cd793e9b4c0e7ba68b7
morita657 <morita657@gmail.com>
2018-05-15 08:06:39 +0900

    Implement wikipedia API

--- pages/TranslatedWord.js
+++ pages/TranslatedWord.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class InputForm extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
+        const title = encodeURIComponent(this.props.output);
+        const wiki = axios.get(`https://ja.wikipedia.org/w/api.php?format=json&action=query&prop=revisions&titles=${title}&rvprop=content`, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            }
+        }).then(response => {
+            return { data: response }
+        })
         return (this.props !== undefined) ? (
             <div>
-                {this.props.output}
+                {/* {this.props.output} */}
+                {this.props.data}
             </div>
         ) : "";
     }
